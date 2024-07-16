c9a4f4e95f13c75db29e1852d055700c54c77ac2
unknown <Prateek Jaiswal>
2024-07-17 00:49:11 +0530

    First commit

---
+++ Cplusplus/Cplusplus.cpp
@@ -0,0 +1,15 @@
+#include "Cplusplus.h"
+int main() {
+	Data d1 = { 11,1,100011,"a" };
+	Data d2 = { 1,2,100011,"a" };
+	Data d3 = { 21,3,100011,"a" };
+	Data d4 = { 7,4,100011,"a" };
+	Cplusplus cpp;
+	cpp.vctVirtual.push_back(&d1);
+	cpp.vctVirtual.push_back(&d2);
+	cpp.vctVirtual.push_back(&d3);
+	cpp.vctVirtual.push_back(&d4);
+	cpp.sort();
+	cpp.copyData();
+	int x = 0;
+}
---
+++ Cplusplus/Cplusplus.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <iostream>
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <execution>
+class Player;
+
+struct Data {
+	int price;
+	int id;
+	int value;
+	std::string input;
+};
+
+class Cplusplus
+{
+private:
+	
+public:
+	std::vector<Data*> vctVirtual, vctVirtual1;
+
+	void sort() {
+		std::sort(std::execution::par,vctVirtual.begin(), vctVirtual.end(), [](Data* data1,Data* data2) {
+			return data1->price > data2->price;	
+			});
+		std::cout << std::is_sorted(vctVirtual.begin(), vctVirtual.end(), [](Data* data1, Data* data2) {return data1->price > data2->price; });
+
+	}
+	void copyData() {
+		std::copy_if(vctVirtual.begin(), vctVirtual.end(),std::back_inserter(vctVirtual1), [](Data* data) {return data->price > 10; });
+		int x = 0;
+	}
+};
+
+
+
+
+
+class Player {
+public:
+	int x = 0;
+};
+
